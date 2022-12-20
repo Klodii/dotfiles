7c26e04a8631aa4fae1d78b21463ad2d63087016
Klaudjan <k.pepaj@hotmail.it>
2022-12-20 15:54:54 +0100

    added code that prevents using arrow keys with nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,14 @@
 require("klaudjan")
 print("Hello")
 vim.opt.relativenumber = true
+-- Try to prevent bad habits like using the arrow keys for movement.
+-- Do this in normal mode...
+vim.cmd("nnoremap <Left>  :echoe 'Use h'<CR>")
+vim.cmd("nnoremap <Right> :echoe 'Use l'<CR>")
+vim.cmd("nnoremap <Up>    :echoe 'Use k'<CR>")
+vim.cmd("nnoremap <Down>  :echoe 'Use j'<CR>")
+-- ...and in insert mode
+vim.cmd("inoremap <Left>  <ESC>:echoe 'Use h'<CR>")
+vim.cmd("inoremap <Right> <ESC>:echoe 'Use l'<CR>")
+vim.cmd("inoremap <Up>    <ESC>:echoe 'Use k'<CR>")
+vim.cmd("inoremap <Down>  <ESC>:echoe 'Use j'<CR>")
