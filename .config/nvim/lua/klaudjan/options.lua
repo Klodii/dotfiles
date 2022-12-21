f20f794c404b6acbcbdc6dbcc1f20928e26cb646
Klaudjan <k.pepaj@hotmail.it>
2022-12-21 14:46:21 +0100

    added some more options and keymap

--- .config/nvim/lua/klaudjan/options.lua
+++ .config/nvim/lua/klaudjan/options.lua
@@ -1,16 +1,21 @@
-vim.opt.autoindent = true  -- New lines inherit the indentation of previous lines.
-vim.opt.expandtab = true  -- Convert tabs to spaces.
-vim.opt.tabstop = 4  -- Indent using 4 spaces
-vim.opt.ruler = true -- Always shows location in file (line#)
-vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
+vim.opt.guicursor = ""  -- To make the cursor fat even in insert mode
 
-vim.opt.smartcase = true -- Automatically switch search to case-sensitive when search query ocntains an uppercase letter
-vim.opt.scrolloff = 1 -- The number of screen lines to keep above and below the cursor
-vim.opt.sidescrolloff = 5 -- The number  of screen columns to keep to the left and right of the cursor.
-vim.opt.laststatus = 2 -- Always display the status basr
-vim.opt.ruler = true -- Always show cursor position
 vim.opt.number = true  -- Show line numbers on the sidebar
 vim.opt.relativenumber = true  -- Show line number on the current line and relative numbers on all other lines
+
+vim.opt.autoindent = true  -- New lines inherit the indentation of previous lines.
+vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent.
+vim.opt.expandtab = true  -- Convert tabs to spaces.
+vim.opt.tabstop = 4  -- Indent using 4 spaces
+
+
+vim.opt.smartcase = true -- Automatically switch search to case-sensitive when search query ocntains an uppercase letter
+
+vim.opt.scrolloff = 8 -- The number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 5 -- The number  of screen columns to keep to the left and right of the cursor.
+vim.opt.colorcolumn = "80"  -- Color a column, used to check when reaceah a certain limit
+vim.opt.laststatus = 2 -- Always display the status basr
+vim.opt.ruler = true -- Always show cursor position
 vim.opt.confirm = true  -- Display a confirmation dialog when closing an unsaved file
 vim.opt.backspace = indent,eol  -- In insert mode you can delete only what you have written in that moment, nothing else
 vim.opt.listchars = {
@@ -18,3 +23,14 @@ vim.opt.listchars = {
     trail = '•'
 }  -- Pair the invisible characters with a simbol
 vim.opt.list = true  -- Show all invisible characters (spaces, tabs, eol, ...) if defined in listchars
+
+vim.opt.swapfile = false  -- Disable swapfile
+vim.opt.backup = false  -- Disable backup files
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+
+vim.opt.hlsearch = true  -- Enable search highlighting
+vim.opt.ignorecase = true  -- Ignore case when searching
+vim.opt.incsearch = true  -- Incremental search that shows partial matches
+vim.opt.smartcase = true  -- Automatically switch search to case-sensitive when search query contains an uppercase letter.
--- .config/nvim/lua/klaudjan/remap.lua
+++ .config/nvim/lua/klaudjan/remap.lua
@@ -12,3 +12,20 @@ vim.keymap.set("i", "<Left>", "<ESC>:echoe 'Use h'<CR>")
 vim.keymap.set("i", "<Right>", "<ESC>:echoe 'Use l'<CR>")
 vim.keymap.set("i", "<Up>", "<ESC>:echoe 'Use k'<CR>")
 vim.keymap.set("i", "<Down>", "<ESC>:echoe 'Use j'<CR>")
+
+-- Move selected lines up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- After moving with C-u or C-d center the screen
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- Center the screen while going on with searched words
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- When paisting a word over another
+-- you don't loose the firt copied word from the paste registry
+-- so you can continuing paste the same word over other words
+vim.keymap.set("x", "<leader>p", "\"_dP")
