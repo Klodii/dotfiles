5f051eb1240b413f3cbce70e9fe7f05dc64bde38
Klaudjan <k.pepaj@hotmail.it>
2022-12-23 14:48:21 +0100

    Changed git find files keymap

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,8 +2,8 @@ local builtin = require('telescope.builtin')
 -- search in all files
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 -- search only in git files
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- search in all files for the string specified 
+vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+-- search in all files for the string specified
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
