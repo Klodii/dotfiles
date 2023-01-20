b007b3beec3e02a97705e555259e74caee5a7ad2
Klaudjan <k.pepaj@hotmail.it>
2023-01-20 12:32:20 +0100

    Updated telescope key map: removed command with a built-in telescope command

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -5,4 +5,4 @@ vim.keymap.set('n', '<leader>sw', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end, { desc = '[s]earch in all files the [w]ord specified' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[s]earch in [h]elp tags' })
-vim.keymap.set('n', '<leader>sk', '<cmd>Telescope keymaps<cr>', { desc = 'search [k]eymap' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'search [k]eymap' })
