95a697e9e3cdd8861762d88e1ad381bb6bdbbed6
Klaudjan Pepaj <klaudjanpepaj@Harithas-MBP.prometeon.com>
2023-02-02 10:21:36 +0100

    Added scirpt that auto updateds packer packages when entered nvim

--- .config/nvim/lua/klaudjan/packer.lua
+++ .config/nvim/lua/klaudjan/packer.lua
@@ -1,9 +1,23 @@
 --, "lua"  This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- Automatic installation of Packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
@@ -59,4 +73,7 @@ return require('packer').startup(function(use)
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
