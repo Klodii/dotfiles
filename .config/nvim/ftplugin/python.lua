8d37b6ec2aacab4242c876c66db8cf47f58a77c6
Klodii <k.pepaj@hotmail.it>
2023-03-28 16:40:29 +0200

    Changed keymaps

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>fd", "?def .*(.*<CR>", { desc = 'go to previouse [f]unction [d]efinition' })
 vim.keymap.set("n", "<leader>cd", "?class .*:<CR>", { desc = 'go to previouse [c]lass [d]efinition' })
-vim.keymap.set("n", "b", "Obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() above' })
+vim.keymap.set("n", "<leader>b", "Obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() above' })
