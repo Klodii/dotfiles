5fb5ec230a347daa0465e5713a0eb9a2c972a1a1
Klodii <k.pepaj@hotmail.it>
2023-04-12 14:42:43 +0200

    Added <leade>B and <leader>b keymap

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>fd", "?def .*(.*<CR>", { desc = 'go to previouse [f]unction [d]efinition' })
 vim.keymap.set("n", "<leader>cd", "?class .*:<CR>", { desc = 'go to previouse [c]lass [d]efinition' })
-vim.keymap.set("n", "<leader>b", "Obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() above' })
+vim.keymap.set("n", "<leader>B", "Obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() above' })
+vim.keymap.set("n", "<leader>b", "obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() below' })
