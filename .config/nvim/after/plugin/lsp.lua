2a09ff6a1ac9f8dcb7b8f08a4fc699ce719e6426
Klaudjan <k.pepaj@hotmail.it>
2023-09-22 17:34:25 +0200

    fix(nvim/lsp): added with the lsp configuration the mason configuration

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,23 @@
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 
-lsp.preset('recommended')
-lsp.setup()
-
+lsp_zero.preset('recommended')
+lsp_zero.setup()
 vim.diagnostic.config({
---    virtual_text = true,
+    virtual_text = false, -- remove the virtualtext next to the error line
+})
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
 
 vim.keymap.set('n', '<leader>r', "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = '[r]ename string all over the project' })
