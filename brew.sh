35f5bf79b88882213d1fe1e3b1cbe2af9cd8486d
Klodii <k.pepaj@hotmail.it>
2023-11-24 08:45:52 +0100

    feat(brew.sh): add new commands

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash -
 
 # Install command-line tools using Homebrew.
 
@@ -9,9 +9,16 @@ brew update
 brew upgrade
 
 # Install other useful binaries.
+brew install alacritty
+brew install tmux
+brew install fzf
 brew install git
 brew install ripgrep
+brew install ksnip
 brew install rename
+brew install mpv
+brew install yt-dlp
+brew install syncthing
 
 # Remove outdated versions from the cellar.
 brew cleanup
