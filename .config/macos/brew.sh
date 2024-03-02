53fa1473f3c14139514e2139375e1cf44b660739
Klodii <k.pepaj@hotmail.it>
2024-03-02 08:11:08 +0100

    feat(alacritty): change font to Hack Nerd Font

--- .config/macos/brew.sh
+++ .config/macos/brew.sh
@@ -25,7 +25,7 @@ brew install git-delta
 
 # Nerd Fonts
 brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-hack-nerd-font
 
 # Remove outdated versions from the cellar.
 brew cleanup
