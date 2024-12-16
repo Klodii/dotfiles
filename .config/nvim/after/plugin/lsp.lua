7b1795f0377e83fac66cb0174f142f81898c88e3
Klodi <k.pepaj@hotmail.it>
2024-12-16 17:11:15 +0100

    feat(nvim): add autoformatting when saving file

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,13 +2,13 @@ local lsp_zero = require('lsp-zero')
 
 lsp_zero.setup()
 vim.diagnostic.config({
-    virtual_text = false, -- remove the virtualtext next to the error line
+  virtual_text = false, -- remove the virtualtext next to the error line
 })
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
@@ -21,3 +21,19 @@ require('mason-lspconfig').setup({
 
 vim.keymap.set('n', '<leader>r', "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = '[r]ename string all over the project' })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then return end
+
+    if client.supports_method('textDocument/fomratting') then
+      -- format the current buffer on save
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+        end,
+      })
+    end
+  end
+})
