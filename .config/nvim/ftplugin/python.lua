ef8c45bf595dbd54544293573b4ca887dc96d227
Klodii <k.pepaj@hotmail.it>
2024-12-17 23:25:44 +0100

    feat(nvim): add keymap to execute tests of the current python file
    Also did some refactoring in the fomratting of patker.lua

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -6,3 +6,20 @@ vim.keymap.set("n", "<leader>fd", "?def .*(.*<CR>", { desc = 'go to previouse [f
 vim.keymap.set("n", "<leader>cd", "?class .*:<CR>", { desc = 'go to previouse [c]lass [d]efinition' })
 vim.keymap.set("n", "<leader>B", "Obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() above' })
 vim.keymap.set("n", "<leader>b", "obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() below' })
+
+-- run tests of the current file
+vim.keymap.set("n",
+  "<leader>t",
+  function()
+    local currentFile = vim.api.nvim_buf_get_name(0)
+    vim.cmd.vnew()      -- open new vertical window
+    vim.cmd.term()      -- open terminal
+    vim.cmd.wincmd("J") -- move the window below
+    vim.api.nvim_win_set_height(0, 15)
+
+    local job_id = vim.bo.channel
+    local enter = "\r\n"
+    vim.fn.chansend(job_id, { "source scripts/aliases" .. enter })
+    vim.fn.chansend(job_id, { "dtest" .. currentFile .. enter })
+  end,
+  { desc = 'Run current file [t]ests' })
--- .config/nvim/lua/klaudjan/packer.lua
+++ .config/nvim/lua/klaudjan/packer.lua
@@ -3,9 +3,9 @@
 -- Automatic installation of Packer
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -20,23 +20,23 @@ return require('packer').startup(function(use)
 
   -- add telescope
   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
   -- telescope plugins must be loaded by telescope
   use { "nvim-telescope/telescope-file-browser.nvim" }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- set colore scheme
   use({
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
       vim.cmd('colorscheme rose-pine')
-      end
+    end
   })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
   use('nvim-treesitter/nvim-treesitter-context')
   use('theprimeagen/harpoon')
@@ -44,31 +44,31 @@ return require('packer').startup(function(use)
   use('tpope/vim-fugitive')
 
   use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+      -- Snippets
+      { 'L3MON4D3/LuaSnip',                 run = "make install_jsregexp" },
+      { 'rafamadriz/friendly-snippets' },
+    }
   }
 
   -- statusline plugin
   use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
   -- indentation
@@ -76,24 +76,24 @@ return require('packer').startup(function(use)
 
   -- gitsigns
   use {
-      'lewis6991/gitsigns.nvim',
-      config = function()
-        require('gitsigns').setup()
-      end
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
   }
 
   -- help you establish good command workflow and habit
   use {
-      "m4xshen/hardtime.nvim",
-      requires = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    "m4xshen/hardtime.nvim",
+    requires = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   }
   -- notification plugin, particularly useful with the hardtime pulgin
   use 'rcarriga/nvim-notify'
 
   -- better todo highlight and related commands, with Telescope and others
   use {
-      "folke/todo-comments.nvim",
-      requires = { "nvim-lua/plenary.nvim" }
+    "folke/todo-comments.nvim",
+    requires = { "nvim-lua/plenary.nvim" }
   }
 
 
