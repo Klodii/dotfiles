574e1c53d9f4367f77b334d8caf3ae96b4363384
Klodii <k.pepaj@hotmail.it>
2024-12-26 19:20:04 +0100

    feat(nvim): started writing info message when file changes
    When the file in the current buffer changes, due to an external command
    (like git pull), the buffer will be reloaded automaticcaly and a
    floating window with a message will appear.

--- .config/nvim/lua/klaudjan/autocmd.lua
+++ .config/nvim/lua/klaudjan/autocmd.lua
@@ -1,22 +1,33 @@
+local utils = require "klaudjan.utils"
+
 -- Automatically delete all trailing whitespaces on save
-vim.api.nvim_create_autocmd("BufWritePre", {command = [[%s/\s\+$//e]]})
+vim.api.nvim_create_autocmd("BufWritePre", { command = [[%s/\s\+$//e]] })
 
 -- Recompile suckless programs Automatically
 vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = {"config.h", "config.def.h"},
-    command = "!sudo make clean install"
+  pattern = { "config.h", "config.def.h" },
+  command = "!sudo make clean install"
 })
 
 -- Recompile latex programs Automatically
 vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = {"*.tex"},
-    command = "!pdflatex %"
+  pattern = { "*.tex" },
+  command = "!pdflatex %"
 })
 
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd("TextYankPost",{
-    desc = 'Highlight when yanking (copying) text',
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- if you are usign tmux, make sure to
+-- set -g focus-events on
+vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
+  desc = "Automatically reload the buffer if the file has changed externally (e.g. due to a `git pull`)",
+  callback = function()
+    utils.open_floating_window({ message = "file changed", height = 1 })
+  end,
 })
--- .config/nvim/lua/klaudjan/utils.lua
+++ .config/nvim/lua/klaudjan/utils.lua
@@ -42,6 +42,36 @@ M.circle = function(char)
   vim.cmd("normal! ciw" .. circled_word)
 end
 
+M.open_floating_window = function(opts)
+  -- Get the current buffer size
+  local width = vim.api.nvim_get_option('columns')
+  local height = vim.api.nvim_get_option('lines')
+
+  local default_row = math.floor(height * 0.4)  -- Window appears near the middle of the screen
+  local default_col = math.floor(width * 0.25)  -- Window is centered horizontally
+  local default_width = math.floor(width * 0.5) -- Window width is 50% of the editor's width
+  local default_height = 3
+  local opts = opts or {}
+
+  -- Define the floating window's size and position
+  local float_opts = {
+    relative = 'editor', -- The floating window is relative to the editor
+    width = opts.width or default_width,
+    height = opts.height or default_height,
+    row = opts.row or default_row,
+    col = opts.col or default_col,
+    style = 'minimal',  -- Minimal window style with no borders or other decorations
+    border = 'rounded', -- Optional: Adds rounded borders
+  }
+
+  -- Open the floating window
+  local buf = vim.api.nvim_create_buf(false, true)                -- Create a new empty buffer
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { opts.message }) -- Set the message in the buffer
+
+  -- Open the window with the buffer content
+  vim.api.nvim_open_win(buf, true, float_opts)
+end
+
 -- the return table is used to permit the retrive of the functions with
 -- :lua require("klaudjan.utils").function_name()
 return M
