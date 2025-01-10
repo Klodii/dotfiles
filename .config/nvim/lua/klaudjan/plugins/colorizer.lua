d4fae66e83b488c9a9d7c03eca3728a48c39ae6d
Klodii <k.pepaj@hotmail.it>
2025-01-10 23:56:41 +0100

    feat(nvim): remove nvim-colorizer plugins
    The plugins didn't work well, so I decide to remove it

--- .config/nvim/lua/klaudjan/plugins/colorizer.lua
+++
@@ -1,8 +0,0 @@
--- to color the rgb strings, like in a css files
--- e.g.:  color: #69BB79;
-return {
-  'norcalli/nvim-colorizer.lua',
-  config = function()
-    require 'colorizer'.setup()
-  end
-}
