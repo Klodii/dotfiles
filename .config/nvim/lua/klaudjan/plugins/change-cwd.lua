25b0ffadd7379e11402cd42bd7c4ae8d6a0f7e81
Klodii <k.pepaj@hotmail.it>
2025-01-14 16:11:15 +0100

    feat(nvim): add root_dir in the change-cwd setup

--- .config/nvim/lua/klaudjan/plugins/change-cwd.lua
+++ .config/nvim/lua/klaudjan/plugins/change-cwd.lua
@@ -3,6 +3,7 @@ return {
     dir = "~/playground/nvim-plugins/change-cwd.nvim",
     dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
     config = function()
+      require "change-cwd".setup({ root_dir = '~/work/forest/' })
       vim.keymap.set("n", "<leader>ff", "<cmd>:lua require('change-cwd').change_working_directory()<cr>",
         { desc = 'change current working directory' })
     end
