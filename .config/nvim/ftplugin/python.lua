4ec025e4dcbb2ea0f6abaac3ce838c32efdfa906
Klodii <k.pepaj@hotmail.it>
2025-01-17 23:15:21 +0100

    refactor(nvim): clean the code

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -7,6 +7,8 @@ vim.keymap.set("n", "<leader>cd", "?class .*:<CR>", { desc = 'go to previouse [c
 vim.keymap.set("n", "<leader>B", "Obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() above' })
 vim.keymap.set("n", "<leader>b", "obreakpoint()<Esc>", { desc = 'Insert [b]reakpoint() below' })
 
+-- run tests of the current file
+
 ---Open a terminal and execute the commands passed
 ---@param commands string[] list of commands to execute
 local open_terminal = function(commands)
@@ -21,34 +23,20 @@ local open_terminal = function(commands)
   end
 end
 
--- run tests of the current file
-vim.keymap.set("n",
-  "<leader>t",
-  function()
-    local execute_test = require 'klaudjan.execute_test'
+---Run the python test of the current file
+---@param cursor_specific? boolean if true run the specific test in which the cursor is on
+local run_tests = function(cursor_specific)
+  cursor_specific = cursor_specific or false
 
-    local file_path = execute_test.get_relative_file_path()
-    local prefix_to_remove = 'application/'
-    if string.sub(file_path, 0, #prefix_to_remove) == prefix_to_remove then
-      file_path = string.sub(file_path, #prefix_to_remove + 1)
-    end
+  local execute_test = require 'klaudjan.cursor_info'
 
-    local enter = "\r\n"
-    local commands = { "source scripts/aliases.sh" .. enter, "dtest " .. file_path .. enter }
-    open_terminal(commands)
-  end,
-  { desc = 'Run current file tests' })
+  local file_path = execute_test.get_relative_file_path()
+  local prefix_to_remove = 'application/'
+  if string.sub(file_path, 0, #prefix_to_remove) == prefix_to_remove then
+    file_path = string.sub(file_path, #prefix_to_remove + 1)
+  end
 
-
-vim.keymap.set("n", "<leader>tt", function()
-    local execute_test = require 'klaudjan.execute_test'
-
-    local file_path = execute_test.get_relative_file_path()
-    local prefix_to_remove = 'application/'
-    if string.sub(file_path, 0, #prefix_to_remove) == prefix_to_remove then
-      file_path = string.sub(file_path, #prefix_to_remove + 1)
-    end
-
+  if cursor_specific == true then
     local names = execute_test.get_class_and_function_name("python")
     if names then
       if names.class_name then
@@ -58,9 +46,12 @@ vim.keymap.set("n", "<leader>tt", function()
         end
       end
     end
+  end
 
-    local enter = "\r\n"
-    local commands = { "source scripts/aliases.sh" .. enter, "dtest " .. file_path .. enter }
-    open_terminal(commands)
-  end,
-  { desc = 'Run current function test' })
+  local enter = "\r\n"
+  local commands = { "source scripts/aliases.sh" .. enter, "dtest " .. file_path .. enter }
+  open_terminal(commands)
+end
+
+vim.keymap.set("n", "<leader>tf", function() run_tests() end, { desc = 'Run file tests' })
+vim.keymap.set("n", "<leader>tc", function() run_tests(true) end, { desc = 'Run test under the cursor' })
--- .config/nvim/lua/klaudjan/cursor_info.lua
+++ .config/nvim/lua/klaudjan/cursor_info.lua
@@ -1,7 +1,3 @@
----@class Names
----@field class_name string?: the class name
----@field function_name string?: the function name
-
 local M = {}
 
 M.class_types = {
@@ -10,30 +6,9 @@ M.class_types = {
   cpp = 'class_specifier',
 }
 
----Open a new buffer, which starts a terminal, in a floating window
----@return integer buffer number
-M.get_floating_terminal = function()
-  local current_width = vim.api.nvim_get_option_value('columns', {})
-  local current_height = vim.api.nvim_get_option_value('lines', {})
-
-  local float_opts = {
-    relative = 'editor',
-    width = math.floor(current_width * 0.5),
-    height = math.floor(current_height * 0.5),
-    row = 5,
-    col = math.floor(current_width * 0.2),
-    style = 'minimal',  -- Minimal window style with no borders or other decorations
-    border = 'rounded', -- Optional: Adds rounded borders
-  }
-
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_call(buf, function()
-    vim.api.nvim_command("term") -- open terminal
-  end
-  )
-  vim.api.nvim_open_win(buf, true, float_opts)
-  return buf
-end
+---@class Names
+---@field class_name string?: the class name
+---@field function_name string?: the function name
 
 ---Return the name of the class and function in which the cursor is
 ---@param language string
