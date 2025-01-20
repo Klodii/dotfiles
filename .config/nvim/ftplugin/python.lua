0a032df91ef7a9e2cff240564585dcda310a488a
Klodii <k.pepaj@hotmail.it>
2025-01-20 16:00:50 +0100

    feat(nvim): add copy_to_clipboard utility function and keymap to copy full path of test in python

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -23,11 +23,15 @@ local open_terminal = function(commands)
   end
 end
 
----Run the python test of the current file
----@param cursor_specific? boolean if true run the specific test in which the cursor is on
-local run_tests = function(cursor_specific)
-  cursor_specific = cursor_specific or false
+---@class TestPath
+---@field file_path string: relative path to of the file
+---@field class_ string: name fo the class in which the cursor is
+---@field function_ string: name fo the function in which the cursor is
+---@field full_test_path string: relative path to file :: class name :: function name
 
+---Return all the information related to the cursor position
+---@return TestPath
+local build_test_path = function()
   local execute_test = require 'klaudjan.cursor_info'
 
   local file_path = execute_test.get_relative_file_path()
@@ -36,22 +40,47 @@ local run_tests = function(cursor_specific)
     file_path = string.sub(file_path, #prefix_to_remove + 1)
   end
 
-  if cursor_specific == true then
-    local names = execute_test.get_class_and_function_name("python")
-    if names then
-      if names.class_name then
-        file_path = file_path .. "::" .. names.class_name
-        if names.function_name then
-          file_path = file_path .. "::" .. names.function_name
-        end
+  local names = execute_test.get_class_and_function_name("python")
+  local full_test_path = file_path
+  if names then
+    if names.class_name then
+      full_test_path = full_test_path .. "::" .. names.class_name
+      if names.function_name then
+        full_test_path = full_test_path .. "::" .. names.function_name
       end
     end
   end
 
+  return {
+    file_path = file_path,
+    class_ = names.class_name,
+    function__ = names.functionname,
+    full_test_path = full_test_path,
+  }
+end
+
+---Run the python test of the current file
+---@param cursor_specific? boolean if true run the specific test in which the cursor is on
+local run_tests = function(cursor_specific)
+  cursor_specific = cursor_specific or false
+
+  local test_path = build_test_path()
+
+  local path_to_execute = test_path.path
+  if cursor_specific == true then
+    path_to_execute = test_path.full_test_path
+  end
+
   local enter = "\r\n"
-  local commands = { "source scripts/aliases.sh" .. enter, "dtest " .. file_path .. enter }
+  local commands = { "source scripts/aliases.sh" .. enter, "dtest " .. path_to_execute .. enter }
   open_terminal(commands)
 end
 
 vim.keymap.set("n", "<leader>tf", function() run_tests() end, { desc = 'Run file tests' })
 vim.keymap.set("n", "<leader>tc", function() run_tests(true) end, { desc = 'Run test under the cursor' })
+vim.keymap.set("n", "<leader>tn", function()
+    local utils = require 'klaudjan.utils'
+    local full_test_path = build_test_path().full_test_path
+    utils.copy_to_clipboard(full_test_path)
+  end,
+  { desc = 'Full test name, related to the cursor position, to be run by pytest' })
--- .config/nvim/lua/klaudjan/utils.lua
+++ .config/nvim/lua/klaudjan/utils.lua
@@ -46,6 +46,23 @@ M.open_floating_window = function(buffer, opts)
   return win
 end
 
+
+--- Copy text in the clipboard
+---@param text string text to copy to the clipboard
+M.copy_to_clipboard = function(text)
+  if not text then
+    vim.notify("No text to copy", vim.log.levels.WARN)
+    return
+  end
+
+  if vim.fn.has('clipboard') == 1 then
+    vim.fn.setreg('+', text)
+    vim.notify("'" .. text .. "' copied")
+  else
+    vim.notify("Clipboard not available", vim.log.levels.ERROR)
+  end
+end
+
 M.circle = function(char)
   -- circle the current word with the string passed
 
