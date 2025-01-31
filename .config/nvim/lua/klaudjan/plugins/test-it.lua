a9ab90b08c9102c5653ebff2f245f2c41232d05c
Klodii <k.pepaj@hotmail.it>
2025-01-31 22:26:08 +0100

    feat(nvim): moved python test command in a new project `test-it.nvim`
    Also add the plugin

---
+++ .config/nvim/lua/klaudjan/plugins/test-it.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "Klodii/test-it.nvim",
+    config = function()
+      require 'test-it'.setup({
+        path_prefix_to_remove = 'application/',
+        test_command = 'source scripts/aliases.sh && dtest ',
+      })
+
+
+      vim.keymap.set("n", "<leader>tf", '<cmd>TestFile<CR>', { desc = 'Run file tests' })
+      vim.keymap.set("n", "<leader>tc", '<cmd>TestIt<CR>', { desc = 'Run test under the cursor' })
+      vim.keymap.set("n", "<leader>to", '<cmd>OpenTestTerm<CR>', { desc = 'Open the test floating window' })
+      vim.keymap.set("n", "<leader>tn", '<cmd>CopyTestPath<CR>',
+        { desc = 'Copy full test name, related to the cursor position' })
+    end
+  }
+}
