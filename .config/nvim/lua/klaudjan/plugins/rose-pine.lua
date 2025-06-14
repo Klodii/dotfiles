19f83559e1e950842402b69bef3fda66e1103bea
Klodii <k.pepaj@hotmail.it>
2025-06-14 15:23:02 +0200

    feat(nvim): change Visual highlight
    This will make easier to visualize the selected lines

--- .config/nvim/lua/klaudjan/plugins/rose-pine.lua
+++ .config/nvim/lua/klaudjan/plugins/rose-pine.lua
@@ -3,6 +3,12 @@ return {
     'rose-pine/neovim',
     as = 'rose-pine',
     config = function()
+      require("rose-pine").setup({
+        highlight_groups = {
+          Visual = { bg = "#9655aa", inherit = false },
+        },
+      })
+
       vim.cmd(
         'colorscheme rose-pine')
     end
