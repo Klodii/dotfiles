efde02265e027cfe643e70e581db9a5efa9c5bd8
Scritch <scritch@mac-personale.station>
2025-08-27 08:35:15 +0200

    feat(macos/brew_extra): add macpass

--- .config/macos/brew_extra.sh
+++ .config/macos/brew_extra.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -
 brew install --cask syncthing
-
 # Set Syncthing up as a service to run automatically at startup
 brew services start syncthing
+
+# Software that opens kdbx files, aka KeePass Password Safe files
+brew install --cask macpass
