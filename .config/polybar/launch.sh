5f41fcc2b5a20b6318fd648b7cf20cfeeecf48eb
Klodii <k.pepaj@hotmail.it>
2025-10-30 10:18:17 +0100

    feat(polybar): add bar also for external screen

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,3 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 polybar main &
+second_monitor=$(xrandr --query | grep 'HDMI-1')
+if [[ $second_monitor = *connected* ]]; then
+    polybar external &
+fi
