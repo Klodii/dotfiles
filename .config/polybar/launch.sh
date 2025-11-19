2bea9416ca822ade6bbccda363bb1b3b5d4e8398
Klodii <k.pepaj@hotmail.it>
2025-11-19 15:02:29 +0100

    feat(polybar): add battery module if a battery is detected

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# Kill existing bars
+killall -q polybar
+
+# Wait until shutdown
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Font installation
 FONT_DIRECTORY="/usr/share/fonts"
 NERDFONT="HackNerdFont-Regular.ttf"
 if [ ! -f "${FONT_DIRECTORY}/${NERDFONT}" ]; then
@@ -10,11 +17,13 @@ if [ ! -f "${FONT_DIRECTORY}/${NERDFONT}" ]; then
     cp -v HackNerdFont-Regular.ttf /usr/share/fonts/
 fi
 
-# Kill existing bars
-killall -q polybar
+# Detect if battery exists
+if ls /sys/class/power_supply/ | grep -q "^BAT"; then
+    export HAS_BATTERY="battery" # battery is the name of the polybar module to use when the battery is there
+else
+    export HAS_BATTERY=""
+fi
 
-# Wait until shutdown
-while pgrep -x polybar >/dev/null; do sleep 1; done
 
 polybar main &
 second_monitor=$(xrandr --query | grep 'HDMI-1')
