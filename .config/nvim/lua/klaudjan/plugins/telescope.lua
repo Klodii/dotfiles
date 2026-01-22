5671bfd8d9f0a8ad24423f8e02759e66e2f643ed
Klodii <k.pepaj@hotmail.it>
2026-01-22 09:16:47 +0100

    fix(nvim/telescope): remove tag from configuration
    In this way we will get always the latest version

--- .config/nvim/lua/klaudjan/plugins/telescope.lua
+++ .config/nvim/lua/klaudjan/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
