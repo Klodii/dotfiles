934808e256285b1053944a524cca75c5950ae7ee
Klodii <k.pepaj@hotmail.it>
2026-01-28 21:52:29 +0100

    feat(polybar): add NotoColorEmoji-Regular as a second font
    To display the emoji

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,18 +5,6 @@ killall -q polybar
 # Wait until shutdown
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Font installation
-FONT_DIRECTORY="/usr/share/fonts"
-NERDFONT="HackNerdFont-Regular.ttf"
-if [ ! -f "${FONT_DIRECTORY}/${NERDFONT}" ]; then
-    # Install the font with icons
-    # to show the icons run gucharmap, this application show
-    # all the fonts that are installed.
-    # To select the icons, choose the font you want and then go to
-    # 'Private Use Area'
-    cp -v HackNerdFont-Regular.ttf /usr/share/fonts/
-fi
-
 desktop_main_monitor=$(xrandr --query | grep 'DP-2')
 if [[ $desktop_main_monitor = *connected* ]]; then
     polybar main &
