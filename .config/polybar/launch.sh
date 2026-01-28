c05e53fe7b27176cfa2e649a8e833b002726383d
Klodii <k.pepaj@hotmail.it>
2026-01-28 20:36:22 +0100

    feat(window manager): add laptop monitor configuraiton

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -17,8 +17,16 @@ if [ ! -f "${FONT_DIRECTORY}/${NERDFONT}" ]; then
     cp -v HackNerdFont-Regular.ttf /usr/share/fonts/
 fi
 
-polybar main &
+desktop_main_monitor=$(xrandr --query | grep 'DP-2')
+if [[ $desktop_main_monitor = *connected* ]]; then
+    polybar main &
+fi
 second_monitor=$(xrandr --query | grep 'HDMI-1')
 if [[ $second_monitor = *connected* ]]; then
     polybar external &
+fi
+
+laptop_monitor=$(xrandr --query | grep 'LVDS-1')
+if [[ $laptop_monitor = *connected* ]]; then
+    polybar laptop &
 fi
