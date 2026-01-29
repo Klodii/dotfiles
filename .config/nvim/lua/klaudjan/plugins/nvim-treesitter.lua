eff4b80a65f4c77917a7e7bdf03b20391491c1c7
Klodii <k.pepaj@hotmail.it>
2026-01-29 09:15:26 +0100

    feat(setup): remove macos directory and move script in upped directory

--- .config/nvim/lua/klaudjan/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/klaudjan/plugins/nvim-treesitter.lua
@@ -8,7 +8,7 @@ return {
       require('nvim-treesitter').install({
         "c", "cpp", "cmake", "comment", "go", "java", "javascript",
         "jsx", "lua", "markdown", "markdown_inline",
-        "python", "rust", "typescript", "tsx", "vim", "vue", "zsh", "bash"
+        "python", "rust", "typescript", "tsx", "vim", "vue", "zsh", "bash", "sh"
       })
 
       vim.api.nvim_create_autocmd("FileType", {
--- .config/setup/macos/brew_extra.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -
-brew install --cask syncthing
-# Set Syncthing up as a service to run automatically at startup
-brew services start syncthing
-
-# Software that opens kdbx files, aka KeePass Password Safe files
-brew install --cask macpass
-# GitHub Readme Instant Preview - Markdown local preview
-brew install grip
